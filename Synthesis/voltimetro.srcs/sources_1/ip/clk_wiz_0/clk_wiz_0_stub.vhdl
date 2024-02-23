4ec10b2f816036223307b2dae357ffb0b99114ec
Pablo Sorrentino <160888432+SorrentinoPablo@users.noreply.github.com>
2024-02-23 14:50:36 -0300

    Add files via upload

---
+++ Synthesis/voltimetro.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_stub.vhdl
@@ -0,0 +1,29 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.1 (win64) Build 2188600 Wed Apr  4 18:40:38 MDT 2018
+-- Date        : Tue Sep 14 14:00:03 2021
+-- Host        : DESKTOP-T67UNDB running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub {c:/Users/Dell/Desktop/Cuatri 7/TP FINAL SORRENTINO
+--               ED1/Codigos/Sintesis/voltimetro.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_stub.vhdl}
+-- Design      : clk_wiz_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35ticsg324-1L
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_wiz_0 is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end clk_wiz_0;
+
+architecture stub of clk_wiz_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,clk_in1";
+begin
+end;
